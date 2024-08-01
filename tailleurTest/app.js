cc76d564fceabec0b91e7eed12f7bb5e754976c1
DS1003 <mouhaleecr7@gmail.com>
2024-08-01 08:23:49 +0000

    feat: add rooter

--- tailleurTest/app.js
+++ tailleurTest/app.js
@@ -10,19 +10,19 @@ app.use(bodyParser.json());
 
 // Get all products
 app.get('/models', (req, res) => {
- //
-try {
-res.json(Post.getAll());
+  //
+  try {
+    res.json(Post.getAll());
 
-  
-} catch (error) {
+
+  } catch (error) {
     throw new Error(error);
-}
+  }
 
 });
 
 app.get('/models/:id', (req, res) => {
- //
+  //
 
 });
 
@@ -31,53 +31,118 @@ app.get('/models/:id', (req, res) => {
 
 // });
 
+app.get('/follow/:id', (req, res) => {
+  //
+  try {
+    res.json(Post.getAll());
+
+
+  } catch (error) {
+    throw new Error(error);
+  }
+
+});
+
+
+app.get('/signaler/:id', (req, res) => {
+  //
+  try {
+    res.json(Post.getAll());
+
+
+  } catch (error) {
+    throw new Error(error);
+  }
+
+});
+
+app.get('/like/:id', (req, res) => {
+  //
+  try {
+    res.json(Post.getAll());
+
+
+  } catch (error) {
+    throw new Error(error);
+  }
+
+});
+
+app.get('/publish/:id', (req, res) => {
+  //
+  try {
+    res.json(Post.getAll());
+
+
+  } catch (error) {
+    throw new Error(error);
+  }
+
+});
+
+app.get('/follow/:id', (req, res) => {
+  //
+  try {
+    res.json(Post.getAll());
+
+
+  } catch (error) {
+    throw new Error(error);
+  }
+
+});
+
+
+
 app.delete('/models/:id', (req, res) => {
- //
+  //
 
 });
 
 app.put('/models/:id', (req, res) => {
   //
- 
- });
 
- app.patch('/models/:id', (req, res) => {
-  //
- 
- });
+});
 
- ////// PROFILE
+app.patch('/models/:id', (req, res) => {
+  //
+
+});
+
+////// PROFILE
 
 
- app.get('/profiles', (req, res) => {
+app.get('/profiles', (req, res) => {
   //
- 
- });
- 
- app.get('/profiles/:id', (req, res) => {
+
+});
+
+app.get('/profiles/:id', (req, res) => {
   //
- 
- });
- 
- app.post('/profiles', (req, res) => {
+
+});
+
+app.post('/profiles', (req, res) => {
   //
- 
- });
- 
- app.delete('/profiles/:id', (req, res) => {
+
+});
+
+app.delete('/profiles/:id', (req, res) => {
   //
- 
- });
- 
- app.put('/profiles/:id', (req, res) => {
-   //
-  
-  });
- 
-  app.patch('/profiles/:id', (req, res) => {
-   //
-  
-  });
-  
+
+});
+
+app.put('/profiles/:id', (req, res) => {
+  //
+
+});
+
+app.patch('/profiles/:id', (req, res) => {
+  //
+
+});
+
+
+
 
 app.listen(4000);
